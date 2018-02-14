cc6c1c36d0cb43ccfc651d18efe63eb1cab16053
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-02-14 10:29:07 +0000

    vowel.py
    to check a given string is vowel or consonent

---
+++ vowel.py
@@ -0,0 +1,9 @@
+v=["a","e","i","o","u"]
+k=input("enter the input")
+if k.isalpha():
+    if k in v:
+        print("vowel")
+    else :
+        print("consonent")
+else:
+    print("enter valid input")

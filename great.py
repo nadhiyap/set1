6f3a598c42034e2c67ab913a2c3b8e788a37e98f
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-02-15 06:20:29 +0530

    great.py
    to find greatest among 3

---
+++ great.py
@@ -0,0 +1,11 @@
+a=int(input("enter number"))
+b=int(input("enter number"))
+c=int(input("enter number"))
+if(a>b and a>c):
+    print(a)
+elif(b>a and b>c):
+    print(b)
+elif(c>a and c>b):
+    print(c)
+else:
+    print("not determined")

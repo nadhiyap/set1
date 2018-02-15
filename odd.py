bad41e530c3f6a371382dd7776ddc7a24a088c44
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-02-15 07:02:26 +0000

    odd.py

---
+++ odd.py
@@ -0,0 +1,12 @@
+i=int(input("enter start"))
+n=int(input("enter a end"))
+if i%2==0:
+    i+=1
+for i in range(i,n+1):
+    if(i%2!=0):
+        print(i)
+    else:
+        pass
+        
+    
+    
